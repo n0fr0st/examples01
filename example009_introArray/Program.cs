53ba86679100737d0b7947e52368e7cf2d17da76
n0fr0st <krasnosheckov.v@yandex.ru>
2023-01-23 04:02:21 +0300

    added new examples from lection

---
+++ example009_introArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result =arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = {11,23,32,4,15,61,37,28,19};
+array[0] = 12;
+Console.WriteLine(array[4]);
+
+/*int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
+*/
---
+++ example010_methodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1,21,3,42,53, 82,46,76,82};
+
+int n = array.Length;
+int find = 82;
+
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
