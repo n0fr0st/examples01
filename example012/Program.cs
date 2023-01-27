8ad4a801fd74c264e5e0c04d5bfd79e4ca9c0a75
n0fr0st <krasnosheckov.v@yandex.ru>
2023-01-27 03:45:19 +0300

    add new exes

---
+++ example012/Program.cs
@@ -0,0 +1,52 @@
+﻿void Method1()
+{
+    Console.WriteLine(" autor ..");
+}
+//Method1();
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+//Method2("text ");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method21("text ", 4);
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+//int year = Method3();
+//Console.WriteLine(year);
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while(i<count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for ( int i = 0; i < count; i++ )
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, " asdf");
+Console.WriteLine(res);
---
+++ example013_for/Program.cs
@@ -0,0 +1,36 @@
+﻿// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+
+string text = "– Я думаю, – сказал князь, улыбаясь, – что, ежели бы вас послали вместо нашего милого Винценгероде, вы бы взяли приступом согласие прусского короля. Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//               012 
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue)
+        {
+             result = result + $"{newValue}";
+        }
+        else 
+        {
+            result = result + $"{text[i]}";
+        };
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+newText = Replace(newText, 'к', 'К' );
+Console.WriteLine(newText);
